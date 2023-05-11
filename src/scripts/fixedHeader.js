84d01555672f50e0349cbe28e8ce382313da11a9
Andrew-Karyaka <akaryaka1997@gmail.com>
2023-05-11 14:43:03 +0600

    refactoring: image height fixed

---
+++ src/scripts/fixedHeader.js
@@ -0,0 +1,8 @@
+// const header = document.querySelector('.header');
+// let headerHeight = header.clientHeight;
+
+// window.scroll(function() {
+//   if(window.scrollTo() > 1){
+//     header.classList.add('header_fixed');
+//   }
+// })
