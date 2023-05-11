834a3251d452271de9cb8ca41eff0ab9ad3b6bbc
Andrew-Karyaka <akaryaka1997@gmail.com>
2023-05-11 19:39:02 +0600

    delete scripts, add border-radius in header

--- src/scripts/fixedHeader.js
+++
@@ -1,8 +0,0 @@
-// const header = document.querySelector('.header');
-// let headerHeight = header.clientHeight;
-
-// window.scroll(function() {
-//   if(window.scrollTo() > 1){
-//     header.classList.add('header_fixed');
-//   }
-// })
